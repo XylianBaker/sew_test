828d800456ee26e196f57ac796a39015fa8fac7f
Jan Kammellander <janschwiderik@gmail.com>
2023-03-20 23:45:21 +0100

    :memo: added docstrings in view.py

--- reelle_zahlen/view.py
+++ reelle_zahlen/view.py
@@ -4,11 +4,19 @@ from controller import Controller
 
 
 class View(QMainWindow):
+    """
+    A view class representing the graphical user interface of the application.
+    """
     input: QDoubleSpinBox
     output: QTextBrowser
     checkbox_complex_numbers: QCheckBox
 
     def __init__(self, controller: Controller):
+        """
+        Initialize the View by loading the user interface and connecting the buttons to the controller.
+
+        :param controller: The controller instance to handle user input.
+        """
         super().__init__()
         uic.loadUi("./ui.ui", self)
         self.button_calculate.clicked.connect(controller.submit)
@@ -16,21 +24,44 @@ class View(QMainWindow):
         self.output.setReadOnly(True)
 
     def reset(self) -> None:
+        """
+        Reset the user interface elements to their initial state.
+        """
         self.input.setValue(0)
         self.output.clear()
         self.checkbox_complex_numbers.setChecked(False)
         self.set_text_statusbar("keine Berechnung durchgeführt")
 
     def set_text_statusbar(self, text: str) -> None:
+        """
+        Set the text of the status bar.
+
+        :param text: The text to display in the status bar.
+        """
         self.statusBar().showMessage(text)
 
     def get_number(self) -> float:
+        """
+        Get the number from the input field.
+
+        :return: The number entered by the user as a float.
+        """
         return self.input.value()
 
     def get_checkbox(self) -> bool:
+        """
+        Check if the complex numbers checkbox is checked.
+
+        :return: True if the complex numbers checkbox is checked, False otherwise.
+        """
         return self.checkbox_complex_numbers.isChecked()
 
     def set_text_output(self, text: str) -> None:
+        """
+        Set the text of the output field.
+
+        :param text: The text to display in the output field.
+        """
         self.output.setText(text)
 
 
