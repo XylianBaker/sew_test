d9a005a238169310732e30bf41b306c18c4965cc
Jan Kammellander <janschwiderik@gmail.com>
2023-03-20 23:42:01 +0100

    :memo: added docstrings in model.py

--- reelle_zahlen/model.py
+++ reelle_zahlen/model.py
@@ -3,16 +3,36 @@ from abc import ABC, abstractmethod
 
 
 class CalculationStrategy(ABC):
+    """
+    An abstract class representing a calculation strategy for finding the natural logarithm of a number.
+    """
+
     def __init__(self):
         self.base_url = "http://127.0.0.1:5000/wurzel"
 
     @abstractmethod
     def get_natural_logarithm(self, number: float) -> str:
+        """
+        Calculate the natural logarithm of the given number.
+
+        :param number: A float representing the number for which the natural logarithm should be calculated.
+        :return: A string containing the formatted result of the natural logarithm calculation.
+        """
         pass
 
 
 class RealNumberStrategy(CalculationStrategy):
+    """
+    A concrete implementation of CalculationStrategy for real numbers.
+    """
+
     def get_natural_logarithm(self, number: float) -> str:
+        """
+        Calculate the natural logarithm of the given real number.
+
+        :param number: A float representing the real number for which the natural logarithm should be calculated.
+        :return: A string containing the formatted result of the natural logarithm calculation.
+        """
         params = {"zahl": str(number), "komplex": "false"}
         response = requests.get(self.base_url, params=params)
 
@@ -26,7 +46,17 @@ class RealNumberStrategy(CalculationStrategy):
 
 
 class ComplexNumberStrategy(CalculationStrategy):
+    """
+    A concrete implementation of CalculationStrategy for complex numbers.
+    """
+
     def get_natural_logarithm(self, number: float) -> str:
+        """
+        Calculate the natural logarithm of the given complex number.
+
+        :param number: A float representing the complex number for which the natural logarithm should be calculated.
+        :return: A string containing the formatted result of the natural logarithm calculation.
+        """
         params = {"zahl": str(number), "komplex": "true"}
         response = requests.get(self.base_url, params=params)
 
@@ -35,6 +65,13 @@ class ComplexNumberStrategy(CalculationStrategy):
 
 
 def get_natural_logarithm(number: float, complex_numbers: bool) -> str:
+    """
+    Calculate the natural logarithm of the given number using the appropriate strategy based on the complex_numbers flag.
+
+    :param number: A float representing the number for which the natural logarithm should be calculated.
+    :param complex_numbers: A boolean indicating whether complex numbers should be considered (True) or not (False).
+    :return: A string containing the formatted result of the natural logarithm calculation.
+    """
     strategy = ComplexNumberStrategy() if complex_numbers else RealNumberStrategy()
     return strategy.get_natural_logarithm(number)
 
