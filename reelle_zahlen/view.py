880b397226a498b2c34b13d46e627e70084567f9
Jan Kammellander <janschwiderik@gmail.com>
2023-03-20 21:01:44 +0100

    :sparkles: finished the view.py

---
+++ reelle_zahlen/view.py
@@ -0,0 +1,34 @@
+from PyQt6.QtWidgets import *
+from PyQt6 import uic
+
+
+class View(QMainWindow):
+    input: QDoubleSpinBox
+    output: QTextBrowser
+    complex_numbers: QCheckBox
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./test.ui", self)
+        self.output.setReadOnly(True)
+
+    def reset(self) -> None:
+        self.input.setValue(0)
+        self.output.clear()
+        self.complex_numbers.setChecked(False)
+
+    def set_text_statusbar(self, text: str) -> None:
+        self.statusbar.showMessage(text)
+
+    def get_text_input(self) -> float:
+        return self.input.value()
+
+    def set_text_output(self, text: str) -> None:
+        self.output.setText(text)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    view = View()
+    view.show()
+    app.exec()
