cfcc088bd7b00e1c97007eefca83002cc6e8b7f2
Jan Kammellander <janschwiderik@gmail.com>
2023-03-20 23:43:27 +0100

    :memo: added docstrings in controller.py

--- reelle_zahlen/controller.py
+++ reelle_zahlen/controller.py
@@ -5,11 +5,21 @@ import model
 
 
 class Controller:
+    """
+    A controller class for managing the interaction between the view and the model.
+    """
+
     def __init__(self):
+        """
+        Initialize the Controller by creating a view instance and displaying it.
+        """
         self.view = view.View(self)
         self.view.show()
 
     def submit(self) -> None:
+        """
+        Calculate the natural logarithm based on user input and display the result in the view.
+        """
         number = self.view.get_number()
         complex_numbers = self.view.get_checkbox()
         logarithm = model.get_natural_logarithm(number, complex_numbers)
@@ -20,6 +30,9 @@ class Controller:
             self.view.set_text_statusbar("Die Berechnung war leider nicht möglich!")
 
     def reset(self) -> None:
+        """
+        Reset the view to its initial state.
+        """
         self.view.reset()
 
 
